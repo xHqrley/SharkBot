b5f3009d400f4fc9f1619c64ba968ebd8b2b90fa
ChaosCantrip <james.donald.02@gmail.com>
2022-04-20 00:11:25 +0100

    Populated Item class

--- definitions/Item.py
+++ definitions/Item.py
@@ -1,2 +1,22 @@
+from definitions import Rarity, Collection, SharkErrors
+
 class Item():
-    pass
+    
+    def __init__(self, itemData):
+        self.id, self.name, self.description = itemData[0:3]
+        self.rarity = Rarity.get(itemData[3])
+        try:
+            self.imageUrl = itemData[4]
+        except IndexError:
+            self.imageUrl = None
+
+    def generate_embed(self):
+        embed = discord.Embed()
+        embed.title = self.name
+        embed.color = self.rarity.colour
+        embed.description = self.description
+        embed.set_footer(text = f"{self.rarity.name} | {self.id}")
+        if self.imageUrl != None:
+            embed.set_thumbnail(url=self.imageUrl)
+
+        return embed
