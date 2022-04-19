93314d6bacc9c17ab2057e34f4cf900a41f19dd7
ChaosCantrip <james.donald.02@gmail.com>
2022-04-20 00:02:20 +0100

    Added 6 main rarities

--- definitions/Rarity.py
+++ definitions/Rarity.py
@@ -7,3 +7,10 @@ class Rarity():
         self.colour = colour
         self.sellPrice = sellPrice
         self.iconName = iconName
+
+common = Rarity("Common", 0x979c9f, "common_item", 5)
+uncommon = Rarity("Uncommon", "uncommon_item", 0x2ecc71, 10)
+rare = Rarity("Rare", 0x6fa8dc, "rare_item", 20)
+legendary = Rarity("Legendary", 0x71368a, "legendary_item", 50)
+exotic = Rarity("Exotic", 0xf1c40f, "exotic_item", 150)
+mythic = Rarity("Mythic", 0xe74c3c, "mythic_item", 500)
